2de569d48ab346b25cf4a2556964cb901bde8c1f
LeoGartner <78305745+LeoGartner@users.noreply.github.com>
2021-01-31 10:27:46 -0300

    Add files via upload

---
+++ fizzbuzz2.py
@@ -0,0 +1,11 @@
+#
+
+numero = int(input("Digite um número: "))
+
+a = numero % 5
+
+if a == 0:
+	print ("Buzz")
+
+else:
+	print (numero)
---
+++ parouimpar.py
@@ -0,0 +1,11 @@
+#
+
+numero = int(input("Digite um número: "))
+
+a = numero % 2
+
+if a == 0:
+	print ("par")
+
+else:
+	print ("ímpar")
